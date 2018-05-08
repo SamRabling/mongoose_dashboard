f8cfefb9b7f4ecff5e04216e7181754ea830cd47
Samantha Rabling <sjarillorabling@gmail.com>
2018-05-08 14:01:37 -0700

    working on server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const flash = require("express-flash");
+var mongoose = require("mongoose");
+var path = require("path");
+
+// sessions
+
+//app use
+
+// mongoose db
+
+// routes
+
+// port
+app.listen(5000, function () {
+    console.log("listening on port 5000");
+});
